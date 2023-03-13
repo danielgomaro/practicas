648b3db73bcefbb503ab0e0ae514c29efd3fc98b
Daniel Gomez <daniel.gomez@radmas.com>
2023-03-13 11:43:44 +0100

    Tour de los Heroes añadido

---
+++ src/app/hero-add/hero-add.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-add',
+  templateUrl: './hero-add.component.html',
+  styleUrls: ['./hero-add.component.css']
+})
+export class HeroAddComponent {
+
+}
